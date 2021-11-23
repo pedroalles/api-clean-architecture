85df8c90044a75fa8c390fb3b93d82d60f96e8b3
pedroalles <pedroalles@gmail.com>
2021-11-22 21:46:20 -0300

    refactor: move production code to their own files

---
+++ src/presentation/routes/login-router.js
@@ -0,0 +1,14 @@
+/* eslint class-methods-use-this: ["error", { "exceptMethods": ["route"] }] */
+const HttpResponse = require('../helpers/http-response');
+
+module.exports = class LoginRouter {
+  route(httpRequest) {
+    if (!httpRequest || !httpRequest.body) return HttpResponse.serverError();
+
+    const { email, password } = httpRequest.body;
+    if (!email) return HttpResponse.badRequest('email');
+    if (!password) return HttpResponse.badRequest('password');
+
+    return false;
+  }
+};
--- src/presentation/routes/login-router.spec.js
+++ src/presentation/routes/login-router.spec.js
@@ -1,39 +1,5 @@
-/* eslint max-classes-per-file: ["error", 3] */
-/* eslint class-methods-use-this: ["error", { "exceptMethods": ["route"] }] */
-
-class MissingParamError extends Error {
-  constructor(paramName) {
-    super(`Missing ${paramName}`);
-    this.name = 'MissingParamError';
-  }
-}
-
-class HttpResponse {
-  static badRequest(paramName) {
-    return {
-      statusCode: 400,
-      body: new MissingParamError(paramName),
-    };
-  }
-
-  static serverError() {
-    return {
-      statusCode: 500,
-    };
-  }
-}
-
-class LoginRouter {
-  route(httpRequest) {
-    if (!httpRequest || !httpRequest.body) return HttpResponse.serverError();
-
-    const { email, password } = httpRequest.body;
-    if (!email) return HttpResponse.badRequest('email');
-    if (!password) return HttpResponse.badRequest('password');
-
-    return false;
-  }
-}
+const LoginRouter = require('./login-router');
+const MissingParamError = require('../helpers/missing-param-error');
 
 describe('Login Router', () => {
   test('Should return 400 if no email is provided', () => {
