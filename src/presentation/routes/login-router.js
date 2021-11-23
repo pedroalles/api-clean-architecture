3301be4dac0a63d5f63493a2b14a780999353529
pedroalles <pedroalles@gmail.com>
2021-11-23 16:17:59 -0300

    test: ensure LoginRouter returns 500 if an invalid AuthUseCase is provided

--- src/presentation/routes/login-router.js
+++ src/presentation/routes/login-router.js
@@ -7,11 +7,16 @@ module.exports = class LoginRouter {
   }
 
   route(httpRequest) {
-    if (!httpRequest || !httpRequest.body) return HttpResponse.serverError();
+    if (!httpRequest || !httpRequest.body
+      || !this.authUseCase || !this.authUseCase.auth
+    ) return HttpResponse.serverError();
+
     const { email, password } = httpRequest.body;
     if (!email) return HttpResponse.badRequest('email');
     if (!password) return HttpResponse.badRequest('password');
+
     this.authUseCase.auth(email, password);
+
     return HttpResponse.unauthorizedError();
   }
 };
