3c1e086c08791081a31c8a6688fa3130190d82d2
pedroalles <pedroalles@gmail.com>
2021-11-24 00:55:49 -0300

    test: ensure LoginRouter returns 400 if an invalid email is provided

--- src/presentation/routes/login-router.spec.js
+++ src/presentation/routes/login-router.spec.js
@@ -1,8 +1,18 @@
-/* eslint max-classes-per-file: ["error", 2] */
+/* eslint max-classes-per-file: ["error", 3] */
 const LoginRouter = require('./login-router');
 const MissingParamError = require('../helpers/missing-param-error');
+const InvalidParamError = require('../helpers/invalid-param-error');
 const UnauthorizedError = require('../helpers/unauthorized-error');
 const ServerError = require('../helpers/server-error');
+
+const makeEmailValidator = () => {
+  class EmailValidatorSpy {
+    isValid() {
+      return this.isEmailValid;
+    }
+  }
+  return new EmailValidatorSpy();
+};
 
 const makeAuthUseCase = () => {
   class AuthUseCaseSpy {
@@ -28,11 +38,14 @@ const makeAuthUseCaseWithError = () => {
 
 const makeSut = () => {
   const authUseCaseSpy = makeAuthUseCase();
+  const emailValidatorSpy = makeEmailValidator();
   authUseCaseSpy.accessToken = 'valid_token';
-  const sut = new LoginRouter(authUseCaseSpy);
+  emailValidatorSpy.isEmailValid = true;
+  const sut = new LoginRouter(authUseCaseSpy, emailValidatorSpy);
   return {
     sut,
     authUseCaseSpy,
+    emailValidatorSpy,
   };
 };
 
@@ -153,5 +166,19 @@ describe('Login Router', () => {
     };
     const httpResponse = await sut.route(httpRequest);
     expect(httpResponse.statusCode).toBe(500);
+  });
+
+  test('Should return 400 if an invalid email is provided', async () => {
+    const { sut, emailValidatorSpy } = makeSut();
+    emailValidatorSpy.isEmailValid = false;
+    const httpRequest = {
+      body: {
+        email: 'invalid_email@mail.com',
+        password: 'any_password',
+      },
+    };
+    const httpResponse = await sut.route(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new InvalidParamError('email'));
   });
 });
