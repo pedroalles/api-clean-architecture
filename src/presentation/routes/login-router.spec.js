4f1a90d6aa769ab487d126f2c647c81b8cb10c11
pedroalles <pedroalles@gmail.com>
2021-11-24 15:51:44 -0300

    test: ensure LoginRouter returns 500 if EmailValidator throws

--- src/presentation/routes/login-router.spec.js
+++ src/presentation/routes/login-router.spec.js
@@ -1,4 +1,4 @@
-/* eslint max-classes-per-file: ["error", 3] */
+/* eslint max-classes-per-file: ["error", 4] */
 const LoginRouter = require('./login-router');
 const MissingParamError = require('../helpers/missing-param-error');
 const InvalidParamError = require('../helpers/invalid-param-error');
@@ -9,6 +9,16 @@ const makeEmailValidator = () => {
   class EmailValidatorSpy {
     isValid() {
       return this.isEmailValid;
+    }
+  }
+  return new EmailValidatorSpy();
+};
+
+const makeEmailValidatorWithError = () => {
+  class EmailValidatorSpy {
+    isValid() {
+      this.error = new Error();
+      throw this.error;
     }
   }
   return new EmailValidatorSpy();
@@ -208,5 +218,19 @@ describe('Login Router', () => {
     const httpResponse = await sut.route(httpRequest);
     expect(httpResponse.statusCode).toBe(500);
     expect(httpResponse.body).toEqual(new ServerError());
+  });
+
+  test('Should return 500 if EmailValidator throws', async () => {
+    const authUseCaseSpy = makeAuthUseCase();
+    const emailValidatorSpy = makeEmailValidatorWithError();
+    const sut = new LoginRouter(authUseCaseSpy, emailValidatorSpy);
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password',
+      },
+    };
+    const httpResponse = await sut.route(httpRequest);
+    expect(httpResponse.statusCode).toBe(500);
   });
 });
